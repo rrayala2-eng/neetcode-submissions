c5a2a205bddda1bceedc26da5b1b810330d23e81
rrayala2-eng <rithikrayala8@gmail.com>
2026-08-12 17:55:54 -0400

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            # decesion to add nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decesion to not to add nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
+
+            
