4e6d59f5a912ef7b863614f54a68deca4b042bf1
rrayala2-eng <rithikrayala8@gmail.com>
2026-08-17 15:47:54 -0400

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeto = {')' : '(',']':'[', '}':'{'}
+
+        for c in s:
+            if c in closeto:
+                if stack and stack[-1] == closeto[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
